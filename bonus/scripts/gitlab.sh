5eb37d3b19df0eda51a1ac684d1ee05044321a72
CodeWithCharles <charles.probe@gmail.com>
2025-12-16 12:33:19 +0100

    + | Bonus part ?

---
+++ bonus/scripts/gitlab.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+# ------------------------------ Sudo safe guard ----------------------------- #
+if [ "$EUID" -ne 0 ]; then
+    echo -e "${RED}Please run as root or use sudo.${NC}"
+    exit 1
+fi
+
+# ------------------------------- Gitlab confs ------------------------------- #
+printf "${GREEN}[GITLAB]${NC} - Installing and launching app...\n"
+kubectl apply -f ./confs/gitlab/namespace.yaml
+kubectl apply -n gitlab -f ./confs/gitlab/volume.yaml > /dev/null
+kubectl apply -n gitlab -f ./confs/gitlab/deployment.yaml > /dev/null
+kubectl apply -n gitlab -f ./confs/gitlab/service.yaml > /dev/null
+
+
+# --------------------- Wait for gitlab pods to be ready --------------------- #
+printf "${GREEN}[GITLAB]${NC} - Waiting for GitLab pods to be ready...\n"
+while true; do
+    status=$(kubectl -n gitlab get pods -l app=gitlab -o jsonpath="{.items[0].status.phase}")
+    if [ "$status" == "Running" ]; then
+        printf "${GREEN}[GITLAB]${NC} - GitLab pods are running.\n"
+        break
+    else
+        printf "${YELLOW}[GITLAB]${NC} - GitLab pod status: $status. Waiting...\n"
+        sleep 5
+    fi
+done
+
+# ------------------------------- Apply ingress ------------------------------ #
+kubectl apply -n gitlab -f ./confs/gitlab/ingress.yaml
+
+# -------------------- Wait for gitlab service to be ready ------------------- #
+printf "${GREEN}[GITLAB]${NC} - Waiting for GitLab service to be ready.\n"
+response="000"
+while [[ "$response" != "302" ]]; do
+    response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/gitlab/ || echo "000")
+    if [[ "$response" == "302" ]]; then
+        break
+    else
+	    printf "${YELLOW}[GITLAB]${NC} - GitLab service status: $response. Waiting...\n"
+	    sleep 5
+    fi
+done
+
+# ------ Get initial root password and save it to .gitlab_password file ------ #
+password=$(sudo kubectl exec -n gitlab $(sudo kubectl get pods -n gitlab -l app=gitlab -o jsonpath='{.items[0].metadata.name}') -- cat /etc/gitlab/initial_root_password | awk '/Password:/ {print $2}')
+echo "$password" > .gitlab_password
+
+# ------------------------------- Final output ------------------------------- #
+echo "gitlab available at: http://localhost/gitlab"
+echo "login: admin, password: $password"
---
+++ bonus/scripts/k3d.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+USERNAME=cpoulain
+
+# ----------------------------- Check permissions ---------------------------- #
+if [ "$EUID" -ne 0 ]; then
+    echo -e "${RED}Please run as root or use sudo.${NC}"
+    exit 1
+fi
+
+# ---------------------- Install and create k3d cluster ---------------------- #
+printf "${GREEN}[K3D]${NC} - Installing and creating k3d cluster...\n"
+if k3d cluster list | grep -q "$USERNAME"; then
+    printf "${YELLOW}[K3D]${NC} - k3d cluster for user ${USERNAME} already exists. Skipping creation.\n"
+    exit 1
+else
+    if ! sudo k3d cluster create $USERNAME --port 80:80@loadbalancer --servers 1 --agents 3; then
+        printf "${RED}[K3D] - Failed to create k3d cluster for user ${USERNAME}.\nIs K3D install ? Is docker service running ?${NC}\n"
+        exit 1
+    fi
+fi
+
+export KUBECONFIG=$(k3d kubeconfig write $USERNAME)
+printf "${GREEN}[K3D] - k3d cluster for user ${USERNAME} created successfully.${NC}\n"
